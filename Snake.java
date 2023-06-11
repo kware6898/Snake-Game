1648467b82af18b9d88ec53756536777192440f8
kware6898 <129717226+kware6898@users.noreply.github.com>
2023-06-10 20:24:06 -0700

    Create Snake.java

---
+++ Snake.java
@@ -0,0 +1,61 @@
+import java.util.LinkedList;
+
+public class Snake{
+    private LinkedList<Cell> snakePartList = new LinkedList<>();
+    private Cell head;
+    private int start=5;
+
+    public Snake(Cell initPos, Board board){
+        for(int i=0; i<start; i++){
+            head = new Cell(initPos.getRow(), initPos.getCol()+1*i);
+            snakePartList.addFirst(head);
+            head.setType(Type.SNAKE);
+            board.setCell(head.getRow(), head.getCol(), Type.SNAKE);
+            // System.out.print(head.getRow());
+            // System.out.print(" ");
+            // System.out.println(head.getCol());
+        }
+    }
+
+    public void grow(Cell nextCell, Board board){
+        head = nextCell;
+        head.setType(Type.SNAKE);
+        board.setCell(head.getRow(), head.getCol(), Type.SNAKE);
+        snakePartList.addFirst(head);
+    }
+
+    void move(Cell nextCell, Board board){
+        Cell tail = snakePartList.removeLast();
+        // tail.setType(Type.EMPTY);
+        board.setCell(tail.getRow(), tail.getCol(), Type.EMPTY);
+
+        head = nextCell;
+        head.setType(Type.SNAKE);
+        board.setCell(head.getRow(), head.getCol(), Type.SNAKE);
+        snakePartList.addFirst(head);
+    }
+
+    // public boolean checkCrash(Cell nextCell){
+    //     if(nextCell.getType()==Type.SNAKE || nextCell.getType()==Type.WALL){
+    //         return true;
+    //     }
+    //     else if()
+    //     return false;
+    // }
+
+    public LinkedList<Cell> getSnakePartList(){
+        return snakePartList;
+    }
+
+    public void setSnakePartList(LinkedList<Cell> snakePartList){
+        this.snakePartList = snakePartList;
+    }
+
+    public Cell getHead(){
+        return head;
+    }
+
+    public void setHead(Cell head){
+        this.head = head;
+    }
+}
